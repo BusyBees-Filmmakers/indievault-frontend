41fb9906e8a7f148336f9d7e63c99b36e11fa097
igrgin <ivo.grgin@outlook.com>
2024-05-13 11:04:56 +0200

    created subscribe page

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -1,28 +1,35 @@
 import {Component, OnInit} from '@angular/core';
+import {SubscriptionPlan} from "./subscriptionPlan";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-subscribe',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    ButtonModule,
+    NgForOf
+  ],
   templateUrl: './subscribe.component.html',
   styleUrl: './subscribe.component.css'
 })
 export class SubscribeComponent implements OnInit{
 
-  private allPlans: {name: string, price: number, points: string[]}[] = [
+  protected allPlans: SubscriptionPlan[] = [
     {name: 'Viewer', price: 5, points: ['Access all the movies']},
     {name: 'Filmmaker', price: 10, points: ['Everything in Viewer', 'Upload your own movies']}
   ]
-
 
   constructor() {
   }
 
   ngOnInit(): void {
-
+    // TODO document why this method 'ngOnInit' is empty
   }
 
-  submit(chosenPlan: number) {
+  subscribe(chosenPlan: number) {
     //TODO: IMPLEMENT SUBSCRIPTION
     //TODO: IN MOCK, REDIRECT TO HOME PAGE
   }
