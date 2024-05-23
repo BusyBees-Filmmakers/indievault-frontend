daa4adaa6ad7539b6fb31a21379fa7c4b4167d2d
igrgin <ivo.grgin@outlook.com>
2024-05-23 16:38:24 +0200

    add homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {SubscribeComponent} from "./subscribe/subscribe.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 
 export const routes: Routes = [
   {path: 'login', title:'Login', component: LoginComponent},
   {path: 'subscribe', title:'Subscribe', component: SubscribeComponent},
+  {path: 'home', title:'Home', component: HomepageComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,61 @@
+import { Component, NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CommonModule } from '@angular/common';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [DialogModule, ButtonModule,InputTextModule, CommonModule],
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+})
+export class HomepageComponent {
+  sections = [
+    {
+      title: 'Selected for You',
+      movies: [
+        { title: 'Movie 1', info: 'Details about Movie 1' },
+        { title: 'Movie 2', info: 'Details about Movie 2' },
+        { title: 'Movie 3', info: 'Details about Movie 1' },
+        { title: 'Movie 4', info: 'Details about Movie 2' },
+        { title: 'Movie 5', info: 'Details about Movie 1' },
+        { title: 'Movie 6', info: 'Details about Movie 2' },
+        // Add more movies
+      ],
+    },
+    {
+      title: 'Horror',
+      movies: [
+        { title: 'Horror Movie 1', info: 'Details about Horror Movie 1' },
+        { title: 'Horror Movie 2', info: 'Details about Horror Movie 2' },
+        // Add more horror movies
+      ],
+    },
+    // Add more sections with movies
+  ];
+
+  displayMovieDialog: boolean = false;
+  selectedMovie: any = null;
+
+  showMovieInfo(movie: any) {
+    this.selectedMovie = movie;
+    this.displayMovieDialog = true;
+  }
+
+  onDialogHide() {
+    this.displayMovieDialog = false;
+    this.selectedMovie = null;
+  }
+
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+}
+}
