37427fe9139fceab5720569adaf0fd6c5243f3c8
aahmed <161947185+aahmedAlgebra@users.noreply.github.com>
2024-12-17 23:48:25 +0100

    added env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyB0139fOO6JaIKsc8irq9XbU9-HMnUk_2M",
+        authDomain: "indievault-a17b9.firebaseapp.com",
+        databaseURL: "https://indievault-a17b9-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "indievault-a17b9",
+        storageBucket: "indievault-a17b9.firebasestorage.app",
+        messagingSenderId: "698981784269",
+        appId: "1:698981784269:web:053b28093b69989cf06784"
+    }
+};
+
+const app = initializeApp(environment.firebaseConfig);
