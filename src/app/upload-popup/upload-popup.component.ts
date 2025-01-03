81423f7557baf6649297e93ada8131a4c5a935ea
igrgin <ivo.grgin@outlook.com>
2025-01-03 15:40:20 +0100

    fixed upload

--- src/app/upload-popup/upload-popup.component.ts
+++ src/app/upload-popup/upload-popup.component.ts
@@ -7,6 +7,7 @@ import {MovieService} from '../service/movie/movie.service';
 import {Movie} from '../common/interface/movie';
 import {ReviewService} from "../service/review/review.service";
 import {Review} from "../common/interface/review";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-upload-popup',
@@ -21,7 +22,8 @@ export class UploadPopupComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private movieService: MovieService,
-    private reviewService: ReviewService
+    private reviewService: ReviewService,
+    private router: Router
   ) {
   }
 
@@ -33,6 +35,9 @@ export class UploadPopupComponent implements OnInit {
   }
 
   onSubmit(): void {
+
+    console.log('Form submitted:', this.uploadForm.value);
+
     const newMovie: Movie = {
       title: 'My Movie',
       id: this.movieService.movieDatabase.length + 1,
@@ -66,5 +71,6 @@ export class UploadPopupComponent implements OnInit {
 
     this.movieService.addMovie(newMovie);
     movieReviews.forEach(review => this.reviewService.addReview(review));
+    this.router.navigate(['home']);
   }
 }
